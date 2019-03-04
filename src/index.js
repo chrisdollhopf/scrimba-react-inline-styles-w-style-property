dd7132904ca206bb63014f40341f26cdea6ec111
chrisdollhopf <42962393+chrisdollhopf@users.noreply.github.com>
2019-03-04 17:24:10 +0100

    Sixth commit
    Inline JS styles to show to dynamically determine styles for how hard coded components display.

--- src/index.js
+++ src/index.js
@@ -2,32 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  const date = new Date();
+  /* Changing the date parameter to see the different JS
+  styles in effect */
+
+  const date = new Date(2018, 8, 2, 10);
   const hours = date.getHours();
   let timeOfDay;
+  /* One reason to use inline styles like this is for
+  something to be more dynamic in which case JS would
+  determine what the styles are. That would look like
+  this: */
+  const styles = {
+    fontSize: "30px"
+  };
 
   if (hours < 12) {
     timeOfDay = "morning";
+    styles.color = "#99FF33";
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon";
+    styles.color = "#FF5733";
   } else {
     timeOfDay = "night";
+    styles.color = "#5E33FF";
   }
-
-  /* To add additional object use another key value pair
-  separated by: , Also when inside of JS without ' ' encaps
-  we cannot use a dash eg - for an inline style object.
-  Instead we use camel case eg: backgroundColor*/
-
-  const styles = {
-    color: "#FF8C00",
-    backgroundColor: "#FF2D00",
-    fontSize: 24 // if using a measurement use "24px"
-  };
-  /* pseudo selectors such as :hover is not able to be
-    used in inline styles, if it is a must use then it is at
-    this point to be done in CSS using className and in the
-    future style libraries and styled components  */
 
   return (
     <div>
