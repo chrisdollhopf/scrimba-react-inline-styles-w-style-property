f2e80293478ccfd21abb4d301b8f476a7ba78d7f
chrisdollhopf <42962393+chrisdollhopf@users.noreply.github.com>
2019-03-04 16:18:31 +0100

    Third commit
    Used javascript to add a date if else and returned  jsx w encapsulated js to return a message.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-    const date = new Date()
-    const hours = date.getHours()
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-    return (
-      <h1>Is is currently about {date.getHours() % 12} o'clock</h1>
-    )
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  return <h1>Good {timeOfDay}!</h1>;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
